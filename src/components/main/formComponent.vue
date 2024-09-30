36f154efcd7c7147fd12647108e1476e4cac501a
Ritzz <154762212+RitzzRiver@users.noreply.github.com>
2024-09-30 15:38:47 -0500

    avances en el proyecto

--- src/components/main/formComponent.vue
+++ src/components/main/formComponent.vue
@@ -3,16 +3,20 @@ export default {
     data() {
         return {
             name: "",
-            surname: "",
             email: "",
             phoneNumber: "",
-            gender: "",
-            commentary: "",
+            birthday: null,
+            gender: null,
+            formation: "",
+            commentary: null,
         };
     },
 
     methods: {
         async submitForm() {
+            const backend =
+                "http://localhost/projects/proyecto-SENA-v2/src/database/API/upload.php";
+
             if (
                 this.commentary.toLowerCase().includes("¿dónde está el gato?")
             ) {
@@ -20,38 +24,40 @@ export default {
                 window.open("/the-forbiden", "_self");
             } else {
                 try {
-                    const response = await fetch(
-                        "http://localhost/projects/proyecto-SENA-v2/src/database/API/upload.php",
-                        {
-                            method: "POST",
-                            headers: {
-                                "Content-Type":
-                                    "application/x-www-form-urlencoded",
-                            },
-                            body: new URLSearchParams({
-                                name: this.name,
-                                surname: this.surname,
-                                email: this.email,
-                                phoneNumber: this.phoneNumber,
-                                gender: this.gender,
-                                commentary: this.commentary,
-                            }),
-                        }
-                    );
+                    const response = await fetch(backend, {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/x-www-form-urlencoded",
+                        },
+                        body: new URLSearchParams({
+                            name: this.name,
+                            email: this.email,
+                            phoneNumber: this.phoneNumber,
+                            birthday: this.birthday,
+                            gender: this.gender,
+                            course: this.course,
+                            commentary: this.commentary,
+                        }),
+                    });
 
+                    console.log(response.body);
+                    
                     if (!response.ok) {
                         throw new Error("Error en la solicitud");
                     }
 
-                    this.name = "";
-                    this.surname = "";
-                    this.email = "";
-                    this.phoneNumber = "";
-                    this.gender = "";
-                    this.commentary = "";
-                    alert("¡Se ha enviado tu información para el registro!");
+                    (this.name = ""),
+                        (this.email = ""),
+                        (this.phoneNumber = ""),
+                        (this.birthday = ""),
+                        (this.gender = ""),
+                        (this.course = ""),
+                        (this.commentary = ""),
+                        alert(
+                            "¡Se ha enviado tu información para el registro!"
+                        );
                 } catch (error) {
-                    alert("error al enviar el formulario :c");
+                    alert(error);
                 }
             }
         },
@@ -67,23 +73,19 @@ export default {
     </div>
 
     <form @submit.prevent="submitForm">
-        <label for="names">Nombres</label>
+        <label for="full name"
+            >Nombre completo <span class="required">*</span></label
+        >
         <input
             type="text"
             v-model="name"
-            placeholder="Ingresa tus nombres"
+            placeholder="Ingresa tu nombre completo"
             required="true"
         />
 
-        <label for="surnames">Apellidos</label>
-        <input
-            type="text"
-            v-model="surname"
-            placeholder="Ingresa tus apellidos"
-            required="true"
-        />
-
-        <label for="email">Correo electrónico</label>
+        <label for="email"
+            >Correo electrónico <span class="required">*</span></label
+        >
         <input
             type="email"
             v-model="email"
@@ -91,7 +93,9 @@ export default {
             required="true"
         />
 
-        <label for="phoneNumber">Número telefónico</label>
+        <label for="phoneNumber"
+            >Número telefónico <span class="required">*</span></label
+        >
         <input
             type="number"
             v-model="phoneNumber"
@@ -99,10 +103,29 @@ export default {
             required="true"
         />
 
+        <label for="birthday">Fecha de nacimiento</label>
+        <input type="date" v-model="birthday" id="birthday" />
+
         <label for="genre">Genero:</label>
-        <select v-model="genre" required="true">
+        <select v-model="gender">
+            <option value="default" hidden="true" selected="true">
+                Selecciona tu genero
+            </option>
             <option value="male">Masculino</option>
             <option value="female">Femenino</option>
+            <option value="other">Otro</option>
+        </select>
+
+        <label for="formation"
+            >Curso a inscribirse <span class="required">*</span></label
+        >
+        <select v-model="formation" id="course" required="true">
+            <option value="default" hidden="true" selected="true">
+                Selecciona el curso que deseas inscribirte
+            </option>
+            <option value="DESARROLLO DE VIDEOJUEGOS Y ENTORNOS INTERACTIVOS">
+                DESARROLLO DE VIDEOJUEGOS Y ENTORNOS INTERACTIVOS
+            </option>
         </select>
 
         <label for="commentary">¿Algún comentario o duda?</label>
@@ -111,6 +134,8 @@ export default {
             placeholder="Escribe aquí la duda que quieras resolver o comentario a realizar"
         ></textarea>
 
+        <div></div>
         <button type="submit" name="upload">¡registrame!</button>
+        <button type="reset">Limpiar formulario</button>
     </form>
 </template>
